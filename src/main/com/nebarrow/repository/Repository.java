1042f717d36c59210644d003f1c39cd99a5c3dba
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-10 12:29:08 +0300

    feat: add repository interface

---
+++ src/main/com/nebarrow/repository/Repository.java
@@ -0,0 +1,17 @@
+package main.com.nebarrow.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<K, E> {
+
+    E save(E entity);
+
+    E update(E entity);
+
+    void delete(K id);
+
+    Optional<E> getById(K id);
+
+    List<E> getAll();
+}
