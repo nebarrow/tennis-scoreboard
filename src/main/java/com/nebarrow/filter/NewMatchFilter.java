281016e8464c46575d7961e630b348d6fb8963cc
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-10 19:18:22 +0300

    feat: add filter to NewMatchServlet.java

---
+++ src/main/java/com/nebarrow/filter/NewMatchFilter.java
@@ -0,0 +1,34 @@
+package com.nebarrow.filter;
+
+import com.nebarrow.dto.request.NewMatchRequest;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import static jakarta.servlet.http.HttpServletResponse.*;
+import java.io.IOException;
+
+public class NewMatchFilter extends HttpFilter {
+    private final String ALLOWED_METHODS = "POST";
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        switch (req.getMethod()) {
+            case "POST" -> handlePostRequest(req, res, chain);
+            default -> {
+                res.setHeader("Allow", ALLOWED_METHODS);
+                res.setStatus(SC_METHOD_NOT_ALLOWED);
+            }
+        }
+    }
+
+    private void handlePostRequest(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+        var request = new NewMatchRequest(
+                req.getParameter("playerOne"),
+                req.getParameter("playerTwo"));
+        req.setAttribute("Players", request);
+        chain.doFilter(req, res);
+    }
+}
