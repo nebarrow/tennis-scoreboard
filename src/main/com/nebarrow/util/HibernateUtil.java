dfda17494e946f3c2903c06324b3102d034b47e4
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-10 11:54:56 +0300

    feat: add get() method to take SessionFactory and flyway

--- src/main/com/nebarrow/util/HibernateUtil.java
+++ src/main/com/nebarrow/util/HibernateUtil.java
@@ -1,18 +1,32 @@
 package src.main.com.nebarrow.util;
 
+import lombok.Getter;
+import org.flywaydb.core.Flyway;
 import lombok.experimental.UtilityClass;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 @UtilityClass
 public class HibernateUtil {
+    @Getter
     private static final SessionFactory sessionFactory;
+    @Getter
+    private static final Configuration configuration;
 
     static {
-        sessionFactory = new Configuration().configure().buildSessionFactory();
+        configuration = new Configuration().configure();
+        sessionFactory = configuration.buildSessionFactory();
+        runMigrations();
     }
 
-    public static SessionFactory get() {
-        return sessionFactory;
+    private void runMigrations() {
+        var flyway = Flyway.configure()
+                .dataSource(
+                        configuration.getProperty("connection.url"),
+                        configuration.getProperty("connection.username"),
+                        configuration.getProperty("connection.password"))
+                .locations("classpath:db/migrations")
+                .load();
+        flyway.migrate();
     }
 }
