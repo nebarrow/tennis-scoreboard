751c9c8a4a959e7e84bbd9971fe7bf65a8b6b4df
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-10 19:17:48 +0300

    feat: create entity for match

---
+++ src/main/java/com/nebarrow/entity/Match.java
@@ -0,0 +1,33 @@
+package com.nebarrow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@Table(name = "Matches")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Match extends BaseEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @JoinColumn(name = "Player1", referencedColumnName = "ID")
+    @ManyToOne
+    Player playerOne;
+
+    @JoinColumn(name = "Player2", referencedColumnName = "ID")
+    @ManyToOne
+    Player playerTwo;
+
+    @JoinColumn(name = "Winner", referencedColumnName = "ID")
+    @ManyToOne
+    Player winner;
+}
