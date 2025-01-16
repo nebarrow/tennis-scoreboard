3979ee2760ebcba699d41118a1ee782712902e54
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-16 16:47:18 +0300

    feat: add GET method

--- src/main/java/com/nebarrow/servlet/NewMatchServlet.java
+++ src/main/java/com/nebarrow/servlet/NewMatchServlet.java
@@ -1,9 +1,11 @@
 package com.nebarrow.servlet;
 
 import com.nebarrow.dto.request.NewMatchRequest;
-import com.nebarrow.entity.CurrentMatch;
-import com.nebarrow.entity.Match;
-import com.nebarrow.entity.Score;
+import com.nebarrow.dto.response.MatchScore;
+import com.nebarrow.dto.response.NewMatchResponse;
+import com.nebarrow.entity.PlayerStats;
+import com.nebarrow.service.NewMatchService;
+import com.nebarrow.service.OngoingMatchService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,24 +13,39 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @WebServlet("/new-match")
 public class NewMatchServlet extends HttpServlet {
+    private final NewMatchService service = new NewMatchService();
+    private final OngoingMatchService matchService = OngoingMatchService.getINSTANCE();
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("new-match.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         var players = (NewMatchRequest) req.getAttribute("Players");
-        var match = new CurrentMatch(
-                UUID.randomUUID(),
-                players.firstPlayerName(),
-                players.secondPlayerName(),
-                new Score(0,0));
-        Map<UUID, CurrentMatch> matches = new HashMap<>();
-        matches.put(match.getID(),match);
-        resp.sendRedirect("/match-score?uuid=" + match.getID());
+        var playersDto = (NewMatchResponse) service.save(players);
+        var playerOneStats = PlayerStats.builder()
+                .id(playersDto.playerOneId())
+                .name(players.firstPlayerName())
+                .build();
+        var playerTwoStats = PlayerStats.builder()
+                .id(playersDto.playerTwoId())
+                .name(players.secondPlayerName())
+                .build();
+
+        var matchScore = MatchScore.builder()
+                .players(new ArrayList<>(List.of(playerOneStats, playerTwoStats)))
+                .build();
+
+
+        var uuid = matchService.addMatch(matchScore);
+        resp.sendRedirect("/match-score?uuid=" + uuid);
     }
 }
