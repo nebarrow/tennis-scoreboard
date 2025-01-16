62f61a0663370f8cb2dc9882ab4ddd03bbb623b7
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-16 16:46:19 +0300

    feat: add method post to the filter

--- src/main/java/com/nebarrow/filter/NewMatchFilter.java
+++ src/main/java/com/nebarrow/filter/NewMatchFilter.java
@@ -11,12 +11,13 @@ import static jakarta.servlet.http.HttpServletResponse.*;
 import java.io.IOException;
 
 public class NewMatchFilter extends HttpFilter {
-    private final String ALLOWED_METHODS = "POST";
+    private final String ALLOWED_METHODS = "POST, GET";
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         switch (req.getMethod()) {
             case "POST" -> handlePostRequest(req, res, chain);
+            case "GET" -> chain.doFilter(req, res);
             default -> {
                 res.setHeader("Allow", ALLOWED_METHODS);
                 res.setStatus(SC_METHOD_NOT_ALLOWED);
