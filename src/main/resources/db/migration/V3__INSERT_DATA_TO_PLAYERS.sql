ae53cf332f684743f23f7ea5fdec33d720a4090a
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-16 16:54:25 +0300

    feat: create sql for insert data into Players table

---
+++ src/main/resources/db/migration/V3__INSERT_DATA_TO_PLAYERS.sql
@@ -0,0 +1,11 @@
+INSERT INTO Players (Name) VALUES
+                                                   ('Anna K.'),
+                                                   ('John D.'),
+                                                   ('Elena S.'),
+                                                   ('Mark R.'),
+                                                   ('Nina L.'),
+                                                   ('Chris M.'),
+                                                   ('Ivan T.'),
+                                                   ('Anton S.'),
+                                                   ('Peter L.'),
+                                                   ('Masha I.');
