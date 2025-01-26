20a32aa81e270ec24d631f9031cdb7a5b5ef1fe3
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-26 22:22:43 +0300

    refactor: rename import name

--- src/main/java/com/nebarrow/repository/PlayerRepository.java
+++ src/main/java/com/nebarrow/repository/PlayerRepository.java
@@ -1,6 +1,6 @@
 package com.nebarrow.repository;
 
-import com.nebarrow.entity.Player;
+import com.nebarrow.model.entity.Player;
 
 public class PlayerRepository extends BaseRepository<Integer, Player> {
     public PlayerRepository(Class<Player> entityClass) {
