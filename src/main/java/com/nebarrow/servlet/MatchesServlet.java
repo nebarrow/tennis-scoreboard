0e1adb1e71b4600247c2b7e0cde8e5a554a46a8a
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-26 22:20:52 +0300

    refactor: move pagination operations to the PaginationService

--- src/main/java/com/nebarrow/servlet/MatchesServlet.java
+++ src/main/java/com/nebarrow/servlet/MatchesServlet.java
@@ -1,7 +1,10 @@
 package com.nebarrow.servlet;
 
 import com.nebarrow.dto.request.MatchWinnerRequest;
+import com.nebarrow.dto.response.MatchResponse;
 import com.nebarrow.service.FinishedMatchPersistenceService;
+import com.nebarrow.service.PaginationService;
+import com.nebarrow.util.ServiceLocator;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,33 +12,46 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 
 @WebServlet("/matches")
 public class MatchesServlet extends HttpServlet {
 
-    private final FinishedMatchPersistenceService finishedMatch = new FinishedMatchPersistenceService();
-    private final int NUMBER_OF_POSTS_PER_PAGE = 5;
+    private static final int DEFAULT_PAGE_NUMBER = 1;
+    private FinishedMatchPersistenceService finishedMatchService;
+    private PaginationService paginationService;
+
+    @Override
+    public void init() {
+        finishedMatchService = ServiceLocator.getService(FinishedMatchPersistenceService.class);
+        paginationService = new PaginationService();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         var filterInput = req.getParameter("filter");
 
-        var matches = filterInput == null ? finishedMatch.getAll() : finishedMatch.getByName(new MatchWinnerRequest(filterInput));
+        var matches = getFilteredMatches(filterInput);
+        var page = getPageNumber(req);
+        var paginatedResult = paginationService.paginate(matches, page);
 
-        int page = Integer.parseInt(req.getParameter("page") == null ? "1" : req.getParameter("page"));
-        int totalMatches = matches.size();
-        int totalPages = (int) Math.ceil((double) totalMatches / NUMBER_OF_POSTS_PER_PAGE);
-
-        int start = (page - 1) * NUMBER_OF_POSTS_PER_PAGE;
-        int end = Math.min(start + NUMBER_OF_POSTS_PER_PAGE, totalMatches);
-        var matchesForPage = matches.subList(start, end);
-
-        req.setAttribute("matches", matchesForPage);
-        req.setAttribute("currentPage", page);
-        req.setAttribute("totalPages", totalPages);
+        req.setAttribute("matches", paginatedResult.items());
+        req.setAttribute("currentPage", paginatedResult.currentPage());
+        req.setAttribute("totalPages", paginatedResult.totalPage());
 
         req.getRequestDispatcher("matches.jsp").forward(req, resp);
+    }
+
+    private int getPageNumber(HttpServletRequest req) {
+        var pageParam = req.getParameter("page");
+        return (pageParam == null || pageParam.isBlank()) ? DEFAULT_PAGE_NUMBER : Integer.parseInt(pageParam);
+    }
+
+    private List<MatchResponse> getFilteredMatches(String filterInput) {
+        return (filterInput == null || filterInput.isBlank())
+                ? finishedMatchService.getAll()
+                : finishedMatchService.getByName(new MatchWinnerRequest(filterInput));
     }
 }
