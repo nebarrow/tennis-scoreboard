68c03fd33eb98f0b7b10d370a27405a44b59a571
nebarrow <101344206+nebarrow@users.noreply.github.com>
2025-01-26 22:46:27 +0300

    refactor: remove isOver method

--- src/main/java/com/nebarrow/service/MatchScoreCalculationService.java
+++ src/main/java/com/nebarrow/service/MatchScoreCalculationService.java
@@ -1,31 +1,23 @@
 package com.nebarrow.service;
 
-import com.nebarrow.dto.response.MatchScore;
-import com.nebarrow.entity.PlayerStats;
+import com.nebarrow.model.common.MatchScore;
+import com.nebarrow.model.common.PlayerStats;
 import com.nebarrow.service.match.MatchScoringProcessor;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class MatchScoreCalculationService {
 
-    private final PlayerStats player;
-    private final MatchScore matchScore;
     private final MatchScoringProcessor matchScoringProcessor = new MatchScoringProcessor();
 
-    public void startMatch() {
-        addPointsToPlayer();
-        loadStatsToGeneralStats();
+    public void calculate(PlayerStats player, MatchScore matchScore) {
+        addPointsToPlayer(player, matchScore);
+        loadStatsToGeneralStats(player, matchScore);
     }
 
-    public boolean isOver() {
-        return player.isWinner();
-    }
-
-    private void addPointsToPlayer() {
+    private void addPointsToPlayer(PlayerStats player, MatchScore matchScore) {
         matchScoringProcessor.process(matchScore, player);
     }
 
-    private void loadStatsToGeneralStats() {
-        matchScore.replacePointsFromPlayerStat(player);
+    private void loadStatsToGeneralStats(PlayerStats player, MatchScore matchScore) {
+       matchScore.replacePointsFromPlayerStat(player);
     }
 }
